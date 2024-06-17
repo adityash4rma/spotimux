8b41c33fc75b4d32a4b2c8b5b6c0f45fa78a63cd
Aditya Sharma <102592458+adityash4rma@users.noreply.github.com>
2024-06-17 16:50:54 +0530

    Add files via upload

--- spotimux.sh
+++ spotimux.sh
@@ -15,31 +15,53 @@ cat << "EOF"
 
 EOF
 
-params=""
-use_newacc=$(termux-dialog confirm -t "Use Default Account?" | jq -r '.text')
 
-if [ "$use_newacc" == "no" ]; then
-    credentials=$(termux-dialog text -i "username:password" -t "Enter Spotify Username & Password:" | jq -r '.text')
-    echo "$credentials"
-    username=$("$credentials" | cut -d ":" -f 1)
-    password=$("$credentials" | cut -d ":" -f 2)
-else
+####### Functions #######
+
+function search_song () {
+    local params=""
+    _search_query=$(termux-dialog text -i "Enter Query: " -t "Search songs:" | jq '.text')
+    local params="${params} -s ${_search_query} --username $1 --password $2"
+    eval "zotify ${params}"
+    exit
+}
+
+function download_song () {
+    local params=""
+    _download_query=$(termux-dialog text -i "Enter URL... " -t "Download: (track/album/playlist/episode/artist)" | jq '.text')
+    local params="${params} -s ${_download_query}"
+    eval "zotify ${params}"
+    exit
+}
+
+#########################
+
+
+
+use_default=$(termux-dialog confirm -t "Use Default Account?" | jq -r '.text')
+
+if [ "$use_default" == "no" ]; then
+    credentials=$(termux-dialog text -i "username:password" -t "Enter Spotify Username & Password:")
+    newcreds=$(echo "$credentials" | jq -r '.text')
+    if [ $(echo "$credentials" | jq -r '.code') != -2 ]; then
+        username=$("$newcreds" | cut -d ":" -f 1)
+        password=$("$newcreds" | cut -d ":" -f 2)
+    else
+        echo "Exited Script: Canceled operation"
+        exit
+    fi
+elif [ "$use_default" == "yes" ]; then
     query=$(termux-dialog sheet -t Queries -v "Search,Download,Download: All Liked Songs,Download: All Songs from Followed artists" | jq -r ".text")
 
-    echo $query
-
     if [ "$query" == "Search" ]; then
-        _search_query=$(termux-dialog text -i "Enter Query: " -t "Search songs..." | jq '.text')
-        params="${params} -s ${_search_query}"
+        search_song
     elif [ "$query" == "Download" ]; then
-        $params+"-d"
+        download_song
     elif [ "$query" == "Download: All Liked Songs" ]; then
         $params+"-l"
     elif [ "$query" == "Download: All Songs from Followed artists" ]; then   
         $params+"-f"
     fi
-
-    eval "zotify ${params}"
 
     #_search=$(zotify -s )
     echo $username 
